ccce11df71086316e39b5e2393e1c7d14f31a110
Anandha Sudhan <rasudhan@gmail.com>
2018-10-06 22:36:17 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -7,7 +7,17 @@ main.addEventListener('click', function(event) {
 });
 
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
+});
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
 });
