98b7d89d58d01ef611e2549d53262d55611a50ab
Anandha Sudhan <rasudhan@gmail.com>
2018-10-06 22:41:43 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -53,12 +53,16 @@ for (let i = 0; i < divs.length; i++) {
 }
 
 divs = document.querySelectorAll('div');
- 
+
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
 }
+
+
+//Demonstrate Stopping Propagated Behaviors with stopPropagation()
+
