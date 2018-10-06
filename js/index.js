2f18edb62e9dd8cbd422b47984da3735ba439349
Anandha Sudhan <rasudhan@gmail.com>
2018-10-06 22:42:38 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -65,4 +65,15 @@ for (let i = 0; i < divs.length; i++) {
 
 
 //Demonstrate Stopping Propagated Behaviors with stopPropagation()
-
+const divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+ 
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
